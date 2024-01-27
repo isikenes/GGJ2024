8474384f0ed419ef6bfc26d677a4acf8b6f12867
emircihanalper <emircihanalper@gmail.com>
2024-01-27 16:38:45 +0300

    1

--- Assets/Scripts/uiScript.cs
+++ Assets/Scripts/uiScript.cs
@@ -7,6 +7,7 @@ public class uiScript : MonoBehaviour
 {
     public GameObject CreditPanel;
     public GameObject OptionsPanel;
+    public GameObject MenuPanel;
     public void baslat()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -18,14 +19,17 @@ public class uiScript : MonoBehaviour
     public void creditscene()
     {
         CreditPanel.gameObject.SetActive(true);
+        MenuPanel.gameObject.SetActive(false);
     }
     public void optionscene()
     {
         OptionsPanel.gameObject.SetActive(true);
+        MenuPanel.gameObject.SetActive(false);
     }
     public void Geributon()
     {
         CreditPanel.gameObject.SetActive(false);
         OptionsPanel.gameObject.SetActive(false);
+        MenuPanel.gameObject.SetActive(true);
     }
 }
