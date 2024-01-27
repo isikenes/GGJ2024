5eb7f0676f89d62e3c5c7a52785bd8aab0efdb22
emircihanalper <emircihanalper@gmail.com>
2024-01-27 23:07:07 +0300

    pause

--- Assets/Scripts/PauseScript.cs
+++ Assets/Scripts/PauseScript.cs
@@ -38,9 +38,5 @@ public class PauseScript : MonoBehaviour
         Time.timeScale = 1f;
         SceneManager.LoadScene(0);
     }
-    public void Restart()
-    {
-        Time.timeScale = 1f;
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-    }
+    
 }
